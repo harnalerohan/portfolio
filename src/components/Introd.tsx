2d9d6bc767eb3310f7d2496bd9bfdba072f7d25d
RON\harna <harnalerohan@gmail.com>
2021-05-16 23:42:04 +0530

    added name changing animation

--- src/components/Introd.tsx
+++ src/components/Introd.tsx
@@ -5,18 +5,29 @@ import logo from "../devGuy.svg"
 
 const Introd = ({bgStatus}: any) => {
  let [name, setName] = useState(true);
+ let [classs, setclasss] = useState(true)
 
  useEffect(() => {
   setTimeout(() => {
-    setName(!name)
+  }, 3400);
+ }, []);
+
+ useEffect(() => {
+  setTimeout(() => {
+  setclasss(!classs)
+  setName(!name)
   }, 2000);
- }, [name]);
+ }, [classs]);
+
+ const handleChange = () => {
+  setclasss(!classs)
+ }
 
   return (
     <div className={bgStatus ? "envelopedBlack" : "envelope"}>
       <div className="intro">
         <p style={{color: bgStatus ? "white" : "black"}}>Welcome to my portfolio...</p>
-        <h1><span style={{color: bgStatus ? "white" : "black"}}>Hello I am </span><span className="name animate">{name ? "Rohan Harnale" : "Web Developer"}</span></h1>
+        <h1><span style={{color: bgStatus ? "white" : "black"}}>Hello I am </span><span className={classs ? "name fadeIn" : "name fadeOut"}>{name ? "Rohan Harnale" : "Web Developer"}</span></h1>
       </div>
       <div className="devGuy">
         <img src={logo} alt="devGuy" />
