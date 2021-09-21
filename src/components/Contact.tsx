a3df1b8d535b2109e3c822b49f9d0043e2a20ede
RON\harna <harnalerohan@gmail.com>
2021-09-22 00:13:46 +0530

    added contact me form backend logic

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,62 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import "../css/contact.css"
 import contact from "../images/contact.svg";
 
 
 const Contact = ({bgStatus}:any) => {
+  const initializeForm = {
+    name: '',
+    email: '',
+    message: '',
+    subject: ''
+  }
+
+  const [formData, setFormData] = useState(initializeForm);
+  const [status, setStatus] = useState(0);
+
+  // 0 = initial hidden stage
+  //1 = loading will appear
+  //2 = success
+  //3 = failure
+
+  const onChange = (event:any) => {
+    const {name,value}= event.target
+    setFormData({...formData,[name]:value})
+  }
+
+  const sendMessage = (e: any) => {
+    e.preventDefault();
+    setStatus(1);
+    let userData={
+      name:formData.name,
+      email: formData.email,
+      message: `Message: ${formData.message} | Email: ${formData.email}`,
+      subject: formData.subject
+    };
+
+    axios.post('http://localhost:5000/app/sendmail', userData)
+      .then(result => {
+        console.log("results", result)
+        if(result.status === 200){
+          // setLoading(false);
+          setStatus(2);
+        }else{
+          setStatus(3)
+        }
+        setTimeout(() => {
+          setStatus(0);
+        }, 2000);
+      }).catch(e => {
+        setStatus(3);
+        setTimeout(() => {
+          setStatus(0);
+        }, 2000);
+        console.log("error", e)
+      })
+  }
+
+
   return (
     <div className="container mb-5">
     <i id="contact"/>
@@ -31,19 +84,29 @@ const Contact = ({bgStatus}:any) => {
         <form className="contactForm link">
           <div className="form-group">
             <label>Full Name</label>
-            <input type="text" className="form-control" placeholder="Enter Name"/>
+            <input name="name" value={formData.name} onChange={onChange} type="text" className="form-control" placeholder="Enter Name"/>
           </div>
           <div className="form-group">
             <label>Email address</label>
-            <input type="email" className="form-control" placeholder="Enter email"/>
+            <input name="email" value={formData.email} onChange={onChange} type="email" className="form-control" placeholder="Enter email"/>
             <small id="emailHelp" className="form-text text-muted">I'll never share your email with anyone else.</small>
           </div>
           <div className="form-group">
-            <label>Message</label>
-            <textarea className="form-control" rows={3}></textarea>
+            <label>Subject</label>
+            <input name="subject" value={formData.subject} onChange={onChange} type="text" className="form-control" placeholder="Enter Subject"/>
           </div>
-          <div className="btn-div">
-          <button type="submit" className="btn btn-dark contact-button">Submit</button>
+          <div className="form-group">
+            <label>Message</label>
+            <textarea name="message" value={formData.message} onChange={onChange} className="form-control" rows={2}></textarea>
+          </div>
+          <div className="col-6 btn-div">
+            <button onClick={sendMessage} type="submit" className="btn btn-dark contact-button">Submit</button>
+          </div>
+          <div style={status === 1 ? {display: "block"} : {display : "none"}} className={"spinner-border"} role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+          <div className="col-6">
+            <i className={status === 2 ? "notification fas fa-check" : status === 3 ?  "notification fas fa-times" : ""}></i>
           </div>
         </form>
       </div>
